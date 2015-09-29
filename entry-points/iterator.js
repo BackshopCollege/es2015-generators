36d54ff272d129317d84faeedd7eb306e53723a4
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2015-09-29 18:16:47 +0100

    add entry point iterator

---
+++ entry-points/iterator.js
@@ -0,0 +1,28 @@
+
+function* iterator() {
+  var i = 0;
+  var stop = false;
+
+  while (!stop) {
+   stop = yield i;
+   i++;
+  }
+}
+
+var iter = iterator();
+var i = iter.next();
+
+while (!i.done) {
+  console.log(i.value);
+  i = iter.next(i.value === 10);
+}
+
+console.log('Using FOR OF loop statement');
+
+function *generator() {
+  for (var i = 0 ; i < 10; i++) yield i;
+}
+
+for (var v of generator()) {
+  console.log(v);
+}
